b55ddfdf17602c47a0f8965a8fc26a7ca715b264
David Parrish <daveparrish@tutanota.com>
2016-03-20 12:58:11 -0400

    Update libraries and add tests for building default decompose environment

---
+++ test/README.md
@@ -0,0 +1,3 @@
+Run the `run_tests_full.bash` script to run tests
+
+Because of expensive environment setup, the `run_tests_full.bash` script wraps the `bats` command. This issue may change this in the future: https://github.com/sstephenson/bats/issues/3
---
+++ test/functions.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Requires DIR global variable which identifies script directory.
+
+function setup_testing_environment() {
+  echo "Setting up Docker testing environment ..."
+  # Create dind daemon with a mount to project.
+  echo "DinD service"
+  testing_env_build=$(docker run --privileged --name decompose-docker-drupal7-testing -d docker:dind)
+  [ ["$?" == "1" ] && echo "$testing_env_build" ]
+
+  # Build decompose environment image
+  echo "Decompose build environment"
+  testing_env_build=$(docker build -t decompose_build_environment ./decompose_environment/.)
+  [ "$?" == "1" ] && echo "$testing_env_build"
+}
+
+function run_tests() {
+  echo "Running BATS tests"
+  bats "$DIR/bats/dind.bats"
+}
+
+function teardown_testing_environment() {
+  echo "Teardown Docker testing environment ..."
+  echo "DinD service"
+  testing_env_cleanup=$(docker rm -f decompose-docker-drupal7-testing)
+  [ "$?" == "1" ] && echo "$testing_env_cleanup"
+
+  echo "decompose build environment"
+  testing_env_cleanup=$(docker rmi -f decompose_build_environment)
+  [ "$?" == "1" ] && echo "$testing_env_cleanup"
+}
